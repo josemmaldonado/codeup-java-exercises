8fc9ddce6bb4f2fa28970e15e0757a5f56ae1d5d
MacOS <josemmaldonado50@gmail.com>
2021-10-25 11:50:38 -0500

    java exercises

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,26 +1,48 @@
 import java.util.Scanner;
 
-public class ConsoleExercises {
-    public static void main (String[] args){
-        double pi = 3.14159;
-        float almostPi = (float) pi;
-        System.out.println("The value of pi is approximately "+ almostPi);
-    Scanner scanner = new Scanner(System.in);
-    System.out.print("Enter an integer: ");
-            int userInput = scanner.nextInt();
-            System.out.println("You entered: --> \"" + userInput + "\"<---");
-        Scanner scanner1 = scanner.nextLine(System.in);
-        String
-
-//This is what happens when I tried to enter a string and not an integer.
-//        Exception in thread "main" java.util.InputMismatchException
-//        at java.base/java.util.Scanner.throwFor(Scanner.java:939)
-//        at java.base/java.util.Scanner.next(Scanner.java:1594)
-//        at java.base/java.util.Scanner.nextInt(Scanner.java:2258)
-//        at java.base/java.util.Scanner.nextInt(Scanner.java:2212)
-//        at ConsoleExercises.main(ConsoleExercises.java:10)
-
-
-
-    }
-}
+//public class ConsoleExercises {
+//    public static void main(String[] args) {
+//        double pi = 3.14159;
+//        System.out.printf("The value of pi is approximately %.2f",pi);
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("\nEnter 3 words: ");
+//        String userInput = scanner.next();
+//        String userInput1 = scanner.next();
+//        String userInput2 = scanner.next();
+//        System.out.println("You entered: -->  \"" +userInput+ " "+userInput1+" " + userInput2 + " \"<---");
+//        Scanner scanner1 = new Scanner(System.in);
+//        System.out.println("Enter a  sentance: ");
+//        String userInputString = scanner1.nextLine();
+//        System.out.println("You entered " + userInputString);
+//        System.out.println("Enter the length of the classroom please?");
+//
+//        String userInput3 = (System.in);
+//
+////        int roomL = parse(userInput3.nextInt());
+//
+//        System.out.println("Enter the width of the classroom please?");
+//
+//        Scanner userInput4 = new Scanner(System.in);
+//
+//
+//        int roomWidth = userInput4.nextInt();
+//
+//        System.out.println("Do you like apples?");
+//
+//        Scanner userInput5 = new Scanner(System.in);
+//
+//        String apples = userInput5.next();
+//
+////        int roomArea = roomWidth*roomL;
+//
+////        int roomPerimeter = (roomWidth*2) + (roomL*2);
+//
+//        System.out.println("Based of your numbers that you enter, the area of your room is " +roomArea);
+//        System.out.println("And the perimeter of the room is "+roomPerimeter+" How you like dem apples!!");
+//
+//
+//
+//
+//
+//    }
+//}
--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,4 +1,4 @@
-public class HelloWord {
+public class HelloWorld {
     public static void main(String[] args) {
 
         int myFavoriteNumber = 500;
