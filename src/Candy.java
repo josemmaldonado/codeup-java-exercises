211ca09a5f6b24eda79bb4dbe15ecd83da33f4d3
MacOS <josemmaldonado50@gmail.com>
2021-11-01 09:43:51 -0500

    :morning exercise

--- src/Candy.java
+++ src/Candy.java
@@ -1,12 +1,36 @@
 public class Candy {
 
-    static public void main(String [] args){
+    public String name;
+
+    public String size;
+
+
+    public Candy(String name){
+        this.name = name;
+        this.size = "unknown";
+    }
+
+    public Candy(String name, String size){
+        this.name = name;
+        this.size = size;
+    }
+    public void trickOrTreat() {
+         System.out.printf("Check out my candy, %s %s", name, size);
+    }
+
+    public static void main(String[] args) {
+
+        Candy butterfingers = new Candy("unwanted");
+        butterfingers.name = "Butterfingers";
+        butterfingers.size = "minture";
+
+        butterfingers.trickOrTreat();
+
 
     }
 };
-public String name (String){
-return name
-};
- public String size (String){
-     return size;
-        };
+
+
+
+
+
