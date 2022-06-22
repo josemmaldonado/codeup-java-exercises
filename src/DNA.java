9df44cacde765343180f9085dccb31a3dca3fd5a
MacOS <josemmaldonado50@gmail.com>
2022-06-22 13:18:56 -0500

    "created method to find protein in dna"

--- src/DNA.java
+++ src/DNA.java
@@ -1,7 +1,40 @@
+// This program determines whether there is a protein 
+// in a strand of DNA.
+
 public class DNA {
 
+    public static String containProtein (String dnaStrand) {
+        
+        int start = dnaStrand.indexOf("ATG"); 
+        int stop = dnaStrand.indexOf("TGA");
+    
+        if (start != -1 && stop != -1 && (stop - start) % 3 == 0) {
+
+            String protein = dnaStrand.substring(start, stop+3);
+
+            return protein + " does contain protein";
+        }
+            return dnaStrand + " does not contain any protein";
+    }
+
+
     public static void main(String[] args) {
-        System.out.println("hello world");
+        
+    //  -. .-.   .-. .-.   .
+    //    \   \ /   \   \ / 
+    //   / \   \   / \   \  
+    //  ~   `-~ `-`   `-~ `-
+
+
+    // DNA strands that I will use to test my program
+    String dna1 = "ATGCGATACGCTTGA";
+    String dna2 = "ATGCGATACGTGA";
+    String dna3 = "ATTAATATGTACTGA";
+
+        System.out.println(containProtein(dna1));
+        System.out.println(containProtein(dna2));
+        System.out.println(containProtein(dna3));
+
     }
 
 }
